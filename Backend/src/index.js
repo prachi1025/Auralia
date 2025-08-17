66674f788ef95d063d3b8f68fb4320214dc536f7
prachi1025 <prachipargal123@gmail.com>
2025-08-18 01:41:42 +0530

    Add MongoDB connection with Mongoose
    Introduced a new db.js module to handle MongoDB connection using Mongoose. The server now connects to the database on startup, improving reliability and separation of concerns.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js"
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
 import adminRoutes from "./routes/admin.route.js"
@@ -28,4 +29,5 @@ app.use("/api/stats", statRoutes)
 app.listen(PORT, () => {
     console.log(`server live on ${PORT}`)
     console.log("http://localhost:5000/")
+    connectDB()
 })
