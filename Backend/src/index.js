f2aa069da65539bfbe0d6e91e44f3c9064624877
prachi1025 <prachipargal123@gmail.com>
2025-08-18 03:13:05 +0530

    Add Mongoose models for Album, Song, and User
    Introduced new Mongoose schemas and models for albums, songs, and users to support database operations. Also updated index.js formatting and added VSCode settings for improved development experience.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
+
 import { connectDB } from "./lib/db.js"
+
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
 import adminRoutes from "./routes/admin.route.js"
