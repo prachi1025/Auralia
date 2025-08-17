5b5757e3f177d46856f17ab072f43d16e9e9f198
prachi1025 <prachipargal123@gmail.com>
2025-08-17 22:37:30 +0530

    Add basic route handlers for API endpoints
    Introduced separate route files for users, auth, admin, songs, albums, and stats, and registered them in the Express app. Each route currently responds to GET requests with a simple message, laying the groundwork for future API development.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,31 @@
 import express from "express"
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js"
+import adminRoutes from "./routes/admin.route.js"
+import songRoutes from "./routes/song.route.js"
+import albumRoutes from "./routes/album.route.js"
+import statRoutes from "./routes/stat.route.js"
 
 dotenv.config()
 
 const app = express()
 const PORT = 5000 || process.env.PORT
 
+//Backend User Routes
+app.use("/api/users", userRoutes)
+//Backend Auth Routes
+app.use("/api/auth", authRoutes)
+//Backend Admin Routes
+app.use("/api/admin", adminRoutes)
+//Backend Songs Routes
+app.use("/api/songs", songRoutes)
+//Backend Albums Routes
+app.use("/api/albums", albumRoutes)
+//Backend Stats Routes
+app.use("/api/stats", statRoutes)
+
 app.listen(PORT, () => {
     console.log(`server live on ${PORT}`)
+    console.log("http://localhost:5000/")
 })
