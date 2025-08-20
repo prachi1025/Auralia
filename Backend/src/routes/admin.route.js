7c5baf6d9c3ae04ec325a160371a371fe1539193
prachi1025 <prachipargal123@gmail.com>
2025-08-20 05:51:35 +0530

    Add deleteSong route to admin API
    Introduced a DELETE endpoint '/delete-song/:id' for admins to remove songs. This enhances admin capabilities for managing song records.

--- Backend/src/routes/admin.route.js
+++ Backend/src/routes/admin.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createSong } from "../controller/admin.controller.js";
+import { createSong, deleteSong } from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
 router.post("/create-song", protectRoute, requireAdmin, createSong)
+router.delete("/delete-song/:id", protectRoute, requireAdmin, deleteSong)
 
 export default router
