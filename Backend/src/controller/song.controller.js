f615a5101e851a6e6b5b06b41bbc5a5ea265955e
prachi1025 <prachipargal123@gmail.com>
2025-08-22 00:44:36 +0530

    Add endpoints for featured, made-for-you, and trending songs
    Introduces three new controller methods and routes to fetch random samples of songs for featured, made-for-you, and trending sections. These endpoints use MongoDB aggregation to return a limited set of song data for each category.

--- Backend/src/controller/song.controller.js
+++ Backend/src/controller/song.controller.js
@@ -10,3 +10,71 @@ export const getAllSongs = async (req, res, next) => {
   }
 }
 
+export const getFeaturedSongs = async (req, res, next) => {
+  try {
+    const featuredSongs = await Song.aggregate([
+      { 
+        $sample: { size: 6 } 
+      }, // Randomly sample 6 songs
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        }
+      }
+    ]);
+
+    res.status(200).json(featuredSongs);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getMadeForYou = async (req, res, next) => {
+  try {
+    const madeForYouSongsSongs = await Song.aggregate([
+      { 
+        $sample: { size: 4 } 
+      }, // Randomly sample 4 songs
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        }
+      }
+    ]);
+
+    res.status(200).json(madeForYouSongsSongs);
+  } catch (error) {
+    next(error);
+  }
+} 
+
+export const getTrendingSongs = async (req, res, next) => {
+  try {
+    const trendingSongs = await Song.aggregate([
+      { 
+        $sample: { size: 4 } 
+      }, // Randomly sample 4 songs
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        }
+      }
+    ]);
+
+    res.status(200).json(trendingSongs);
+  } catch (error) {
+    next(error);
+  }
+} 
--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getAllSongs } from "../controllers/song.controller.js";
+import { getAllSongs, getFeaturedSongs, getMadeForYou, getTrendingSongs } from "../controllers/song.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
 router.get("/", protectRoute, requireAdmin, getAllSongs)
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYou)
+router.get("/trending", getTrendingSongs)
 
 export default router
