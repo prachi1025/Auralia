a8820200c8f85c0c41948eaa30f0e6b90f797430
prachi1025 <prachipargal123@gmail.com>
2025-08-22 01:00:00 +0530

    Add getAllUsers controller and secure user route
    Introduced getAllUsers controller to fetch all users except the current user. Updated user route to use protectRoute middleware and the new controller for GET requests.

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
+import { protectRoute } from "../middleware/auth.middleware.js"
+import { getAllUsers } from "../controllers/user.controller.js"
 
 const router = Router()
 
-router.get("/", (req, res) => {
-    res.send("user route with get method")
-})
+router.get("/", protectRoute, getAllUsers)
+
+// todo: getMessages between users 
 
 export default router 
